42a6012996651808309c5c8f9bc0d6b64d664557
Bugra GURAY @ CryptedBytes Mobile Development <bugra21@outlook.com>
2022-03-03 02:13:37 +0300

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+generateProblem();
+//   let problemform = document.getElementById("problemform");
+//   problemform.addEventListener('submit', (event) => {
+//     generateProblem();
+// });
+var a, b;
+
+var inputfield = document.getElementById("answerinput");
+inputfield.addEventListener('input', () =>{
+    let problemlabel = document.getElementById("problemlabel");
+    let inputlen = inputfield.value.length
+    console.log("Length of input: " + inputlen);
+    console.log("text changed");
+    if(inputlen > 0)  generateProblem();
+});
+
+function generateProblem() {
+    let problemlabel = document.getElementById("problemlabel");
+    let input = document.getElementById("answerinput").value;
+   // alert(input);
+  
+    a = Math.floor(Math.random()* 101) //0..100
+    b = Math.floor(Math.random()* 101) //0..100
+    problemlabel.textContent = `${a} + ${b}`;
+
+    //console.log("args: " + arguments[0])
+    if(arguments[0] == "onclick" && input != a+b){
+        alert("Wrong!!!!!!");
+    }
+
+}
